7208bd79f20675b492029fc1f3c165f389ce69ab
Callie Hart <callie_miller@hotmail.com>
2018-11-16 17:41:32 -0600

    first push

---
+++ config/orm.js
@@ -0,0 +1,18 @@
+var connection = require("../config/connection.js");
+
+var orm = {
+
+    function selectAll(){
+
+    };
+
+    function insertOne(){
+
+    };
+
+    function updateOne(){
+
+    };
+};
+
+module.exports = orm;
---
+++ controllers/burgerController.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var router = express.Router();
+
+var burger = require("../models/burger.js");
+
+router.get("/", function(req, res){
+
+});
+
+router.post("/api/burgers", function(req, res){
+
+});
+
+module.exports = router;
