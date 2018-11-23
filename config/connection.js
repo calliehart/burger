02039c9add941f328ca9c86f6afa43183e460b87
Callie Hart <callie_miller@hotmail.com>
2018-11-23 13:02:53 -0700

    second try

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
+let connection;
+const dotenv = require('dotenv').config();
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL) {
+  console.log("Jaws");
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  console.log("Not Jaws");
+  console.log(dotenv);
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "C@lm2011",
+  password: process.env.SECRET_KEY,
   database: "burgers_db"
 });
+};
 
 connection.connect(function(err) {
     if (err) {
